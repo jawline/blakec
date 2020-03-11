8aac34eaed628437859b7450018e44a4d83db567
Blake Loring <blake@parsed.uk>
2020-03-11 13:02:33 +0800

    Finish prepend and begin with LL unit testing

--- src/blll.h
+++ src/blll.h
@@ -1,37 +1,54 @@
 #ifndef _BLAKE_LINKED_LIST_H_
 #define _BLAKE_LINKED_LIST_H_
 #include <stddef.h>
+#include <stdlib.h>
 
 #define LINKED_LIST_TYPE(NAME, TYPE) \
-typedef struct NAME { \
-  struct NAME##_elem* start; \
-  struct NAME##_elem* end; \
-}; \
 typedef struct NAME##_elem { \
   TYPE data; \
   struct NAME##_elem* next; \
   struct NAME##_elem* prev; \
+};\
+typedef struct NAME { \
+  struct NAME##_elem* start; \
+  struct NAME##_elem* end; \
+  size_t size; \
 };
 
 #define LINKED_LIST_INIT(NAME) void NAME##_init(struct NAME* l) { \
   l->start = NULL; \
   l->end = NULL; \
+  l->size = 0; \
 }
 
-#define LINKED_LIST_PREPEND(NAME, TYPE) void NAME##_prepend(struct NAME** l, TYPE v) { \
-  struct NAME_elem* new_node = malloc(sizeof(struct NAME)); \
+#define LINKED_LIST_PREPEND(NAME, TYPE) void NAME##_prepend(struct NAME* l, TYPE v) { \
+  struct NAME##_elem* new_node = malloc(sizeof(struct NAME##_elem)); \
   new_node->prev = NULL; \
-  new_node->next = *l; \
+  new_node->next = l->start; \
   new_node->data = v; \
-  if (*l) { \
-    *l->prev = new_node; \
+  if (l->start) { \
+    l->start->prev = new_node; \
   } \
-  *l = new_node; \
+  l->start = new_node; \
+  if (!l->end) { \
+    l->end = new_node; \
+  } \
+  l->size += 1; \
+}
+
+#define LINKED_LIST_ITER(NAME) struct NAME##_elem* NAME##_iter(struct NAME* l) { \
+  return l->start; \
+}
+
+#define LINKED_LIST_SIZE(NAME) size_t NAME##_size(struct NAME* l) { \
+  return l->size; \
 }
 
 #define CREATE_LINKED_LIST(NAME, TYPE) \
   LINKED_LIST_TYPE(NAME, TYPE) \
   LINKED_LIST_INIT(NAME) \
-  LINKED_LIST_PREPEND(NAME, TYPE)
+  LINKED_LIST_PREPEND(NAME, TYPE) \
+  LINKED_LIST_ITER(NAME) \
+  LINKED_LIST_SIZE(NAME)
 
 #endif
--- test/ll.c
+++ test/ll.c
@@ -1,8 +1,17 @@
 #include "blll.h"
+#include <assert.h>
 
 CREATE_LINKED_LIST(int_list, int);
 
-int main(int argc, char** argv) {
+void test_prepend() {
   struct int_list t;
   int_list_init(&t);
+  int_list_prepend(&t, 53);
+  assert(int_list_size(&t) == 1);
+  assert(int_list_iter(&t) != NULL);
+  assert(int_list_iter(&t)->data == 53);
+}
+
+int main(int argc, char** argv) {
+  test_prepend();
 }
