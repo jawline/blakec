da6d941e6cdac9b7b41b8f01c292882cc42f12a0
Blake Loring <blake@parsed.uk>
2020-03-13 08:51:48 +0800

    Concatenate dynamic allocated arrays

--- src/bllist.h
+++ src/bllist.h
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <string.h>
 
 /**
  * Dynamically allocated array structure.
@@ -11,14 +12,14 @@
 #define DYNAMIC_ARRAY_TYPE(NAME, TYPE) struct NAME { \
   TYPE* data; \
   size_t current; \
-  size_t max_capacity; \
+  size_t capacity; \
   size_t shrink_at; \
 };
 
 #define DYNAMIC_ARRAY_INIT(NAME) void NAME##_init(struct NAME* l) { \
   l->data = NULL; \
   l->current = 0; \
-  l->max_capacity = 0; \
+  l->capacity = 0; \
   l->shrink_at = 0; \
 }
 
@@ -28,8 +29,8 @@
 }
 
 #define DYNAMIC_ARRAY_ADJUST_SHRINK(NAME, TYPE, SIZE) \
-  if (l->max_capacity > SIZE) { \
-    l->shrink_at = (l->max_capacity / 3); \
+  if (l->capacity > SIZE) { \
+    l->shrink_at = (l->capacity / 3); \
   } else { \
     l->shrink_at = 0; \
   }
@@ -38,13 +39,13 @@
  * Amortised cost increase and shrink. Double capacity - don't grow linearly.
  */
 #define DYNAMIC_ARRAY_INCREASE(NAME, TYPE, SIZE) void NAME##_increase(struct NAME* l) { \
-  if (l->max_capacity == 0) { \
-    l->max_capacity += SIZE; \
+  if (l->capacity == 0) { \
+    l->capacity += SIZE; \
   } else { \
-    l->max_capacity += l->max_capacity; \
+    l->capacity += l->capacity; \
   } \
   DYNAMIC_ARRAY_ADJUST_SHRINK(NAME, TYPE, SIZE) \
-  l->data = realloc(l->data, sizeof(TYPE) * l->max_capacity); \
+  l->data = realloc(l->data, sizeof(TYPE) * l->capacity); \
 }
 
 /**
@@ -53,14 +54,14 @@
  */
 #define DYNAMIC_ARRAY_SHRINK(NAME, TYPE, SIZE) void NAME##_shrink(struct NAME* l) { \
   if (l->current < l->shrink_at) { \
-    l->max_capacity /= 2; \
-    l->data = realloc(l->data, l->max_capacity * sizeof(TYPE)); \
+    l->capacity /= 2; \
+    l->data = realloc(l->data, l->capacity * sizeof(TYPE)); \
     DYNAMIC_ARRAY_ADJUST_SHRINK(NAME, TYPE, SIZE) \
   } \
 }
 
 #define DYNAMIC_ARRAY_PUSH(NAME, TYPE) void NAME##_push(struct NAME* l, TYPE v) { \
-  if (l->current >= l->max_capacity) { \
+  if (l->current >= l->capacity) { \
     NAME##_increase(l); \
   } \
   *(l->data + l->current) = v; \
@@ -74,10 +75,18 @@
   return r; \
 }
 
+#define DYNAMIC_ARRAY_CONCAT(NAME, TYPE, SIZE) TYPE NAME##_concat(struct NAME* l, struct NAME* l2) { \
+  l->capacity = (l->current + l2->current) + SIZE; \
+  l->data = realloc(l->data, sizeof(TYPE) * l->capacity); \
+  memcpy(l->data + l->current, l2->data, l2->current * sizeof(TYPE)); \
+  l->current += l2->current; \
+  DYNAMIC_ARRAY_ADJUST_SHRINK(NAME, TYPE, SIZE); \
+}
+
 #define DYNAMIC_ARRAY_SIZE(NAME) size_t NAME##_size(struct NAME* l) { \
   return l->current; \
 }
 
-#define DYNAMIC_ARRAY(name, type, block_size) DYNAMIC_ARRAY_TYPE(name, type); DYNAMIC_ARRAY_INIT(name) DYNAMIC_ARRAY_FREE(name) DYNAMIC_ARRAY_INCREASE(name, type, block_size) DYNAMIC_ARRAY_PUSH(name, type) DYNAMIC_ARRAY_SHRINK(name, type, block_size) DYNAMIC_ARRAY_POP(name, type) DYNAMIC_ARRAY_SIZE(name)
+#define DYNAMIC_ARRAY(name, type, block_size) DYNAMIC_ARRAY_TYPE(name, type); DYNAMIC_ARRAY_INIT(name) DYNAMIC_ARRAY_FREE(name) DYNAMIC_ARRAY_INCREASE(name, type, block_size) DYNAMIC_ARRAY_PUSH(name, type) DYNAMIC_ARRAY_SHRINK(name, type, block_size) DYNAMIC_ARRAY_POP(name, type) DYNAMIC_ARRAY_CONCAT(name, type, block_size) DYNAMIC_ARRAY_SIZE(name)
 
 #endif
